3fe99ec15ac1b60ec438eee48cc84d996e72256e
Peter <Petertheking20122@gmail.com>
2024-02-03 16:57:59 +0100

    Refactor user management component to display user data and add promote/ban buttons

--- Frontend/src/app/admin-components/user-management/user-management.component.ts
+++ Frontend/src/app/admin-components/user-management/user-management.component.ts
@@ -1,15 +1,39 @@
 import { Component } from '@angular/core';
-
+import { UserManagementService } from '../../services/user.service'; // Importa il servizio per ottenere i dati degli utenti
+import { User } from '../../model/user';
 @Component({
   selector: 'app-user-management',
   templateUrl: './user-management.component.html',
-  styleUrl: './user-management.component.css'
+  styleUrl: './user-management.component.css',
 })
 export class UserManagementComponent {
-  userList: any[] = [
-    ["Cazzo", "Culo", "Tette", ],
-    ["asad", "asdas", "sadas"],
-    ["Cane", "asff","sadsa"]
-  ]
 
+
+  
+
+  constructor(private userService: UserManagementService) {}
+  userList: User[] = []
+
+  ngOnInit(): void {
+     this.getUsers();
+  }
+  
+  getUsers(){
+    this.userService.getUsers().subscribe({
+      next: (response: User[]) => { // Use User[] as the type for response
+        return response;
+      },
+      error: (error: any) => {
+        console.log('There was an error!', error);
+        return []
+      }
+    });
+  }
+  promoteUser(user: any) {
+    console.log('Utente promosso:', user);
+  }
+
+  banUser(user: any) {
+    console.log('Utente bannato:', user); 
+  }
 }
