5dc8c2f6b04a96588bb44de4ec57abb9733d0dee
Rhaegal222 <rhaegal222@gmail.com>
2024-02-05 04:42:57 +0100

    Aggiornamenti visualizzazione e gestione prodotti

--- Frontend/src/app/common-components/tools-bar/tools-bar.component.ts
+++ Frontend/src/app/common-components/tools-bar/tools-bar.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+var box : any;
+var boxItem : any;
+var boxItemBody : any;
+var boxItemImg : any;
+var boxItemFooter : any;
+var boxItemButtons : any;
+
 @Component({
   selector: 'app-tools-bar',
   templateUrl: './tools-bar.component.html',
@@ -8,6 +15,30 @@ import { Component } from '@angular/core';
     '../../styles/buttons.css',
   ]
 })
+
 export class ToolsBarComponent {
+  constructor() { }
+
+  isActive : boolean = true;
+
+  changeView() {
+    this.isActive = !this.isActive;
+
+    box = document.getElementById('box');
+    
+    if (box == null) return;
+
+    console.log(box);
+
+    if(this.isActive) {
+      box.classList.toggle('list', false);
+      box.classList.toggle('grid', true);
+    }
+    else {
+      box.classList.toggle('list', true);
+      box.classList.toggle('grid', false);
+    }
+
+  }
 
 }
--- Frontend/src/app/storekeeper-components/product-management/product-management.component.ts
+++ Frontend/src/app/storekeeper-components/product-management/product-management.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../model/product';
 
 @Component({
   selector: 'app-product-management',
   templateUrl: './product-management.component.html',
-  styleUrl: './product-management.component.css'
+  styleUrls: [
+    './product-management.component.css',
+    '../../styles/buttons.css'
+  ]
 })
+
 export class ProductManagementComponent {
 
+  constructor(private productService: ProductService) { }
+
+  products: Product[] = [];
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  isAnImage(image : string): boolean {
+    if(image == null || image == "" || !image.startsWith('data:image/')){
+      return false;
+    }
+    return true;
+  }
+
+  getProducts(){
+    this.productService.getProducts().subscribe({
+      next: (data) => {
+        this.products = data;
+      },
+      error: (error) => {
+        console.error(error)
+      }
+    });
+  }
 }
