2da974e2bb84cb62086f05d1459406ec1013d565
Rhaegal222 <rhaegal222@gmail.com>
2024-02-08 16:53:06 +0100

    Aggiornamenti alla gestione degli utenti e alla dashboard

--- Frontend/src/app/admin-components/user-management/user-management.component.ts
+++ Frontend/src/app/admin-components/user-management/user-management.component.ts
@@ -1,40 +1,92 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter, Input } from '@angular/core';
+import { Observable } from 'rxjs';
 import { UserManagementService } from '../../services/user.service'; // Importa il servizio per ottenere i dati degli utenti
 import { User } from '../../model/user';
+
 @Component({
   selector: 'app-user-management',
   templateUrl: './user-management.component.html',
   styleUrls: [
     './user-management.component.css',
-    '../../styles/container.css',
-    '../../styles/buttons.css'
+    '../../styles/grid.css',
+    '../../styles/list.css',
+    '../../styles/buttons.css',
   ]
 })
 export class UserManagementComponent {
 
   constructor(private userService: UserManagementService) {}
   
-  userList: User[] = [];
+  users: User[] = [];
+  selectedUser: User | undefined;
+  length: number = 0;
+  userDetailsWindow: boolean = false;
 
   ngOnInit(): void {
-     this.getUsers();
+     this.observeUserListLenght();
+      this.getUsers();
   }
-  
+
+  observeUserListLenght(){
+    const observable = new Observable((observer) => {
+      observer.next(this.users.length);
+    });
+
+    observable.subscribe((length: unknown) => {
+      if (typeof length === 'number') {
+        const anyLength: any = length;
+        this.length = anyLength;
+      }
+    });
+  }
+
+  searchValue: string = '';
+  role: string = 'all';
+
+  /*
+
+  onSearch(eventData: onSearchEventData) {
+    this.searchValue = eventData.searchValue;
+    this.role = eventData.role;
+    if (this.searchValue === "" && this.role === "all") {
+      this.getUsers();
+    } else {
+      this.userService.getUsers(this.searchValue, this.role).subscribe({
+        next: (data) => {
+          this.users = data;
+        },
+        error: (error: any) => {
+          console.log('There was an error!', error);
+        }
+      });
+    }
+  }
+  */
+
+  OnOpenUserDetails(user: User) {
+    this.selectedUser = user;
+    this.userDetailsWindow = true;
+  }
+
+  OnCloseUserDetails() {
+    this.userDetailsWindow = false;
+  }
+
   getUsers(){
     this.userService.getUsers().subscribe({
       next: (data) => {
-        this.userList = data;
+        this.users = data;
       },
       error: (error: any) => {
         console.log('There was an error!', error);
       }
     });
   }
-  promoteUser(user: any) {
+  onPromote(user: User) {
     console.log('Utente promosso:', user);
   }
 
-  banUser(user: any) {
+  onBan(user: User) {
     console.log('Utente bannato:', user); 
   }
 }
--- Frontend/src/app/services/product.service.ts
+++ Frontend/src/app/services/product.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs'
 import { Product } from '../model/product';
-import { get } from 'http';
 
 @Injectable({
   providedIn: 'root'
