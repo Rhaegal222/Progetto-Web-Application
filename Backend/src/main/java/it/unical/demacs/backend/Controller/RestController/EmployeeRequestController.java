5613143498c46ef20285f48b635c5660df930046
marteena <martinapiaimbrogno@gmail.com>
2024-02-08 17:02:19 +0100

    Pulizia Controller

--- Backend/src/main/java/it/unical/demacs/backend/Controller/RestController/EmployeeRequestController.java
+++ Backend/src/main/java/it/unical/demacs/backend/Controller/RestController/EmployeeRequestController.java
@@ -1,13 +1,9 @@
 package it.unical.demacs.backend.Controller.RestController;
 
-import it.unical.demacs.backend.Persistence.Model.EmployeeRequest;
-import it.unical.demacs.backend.Service.AuthenticationService;
 import it.unical.demacs.backend.Service.EmployeeRequestService;
-import it.unical.demacs.backend.Service.Request.LoginRequest;
 import it.unical.demacs.backend.Service.Request.SendReqRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,5 +17,4 @@ public class EmployeeRequestController {
     public ResponseEntity<?> sendRequest(@RequestBody SendReqRequest sendReqRequest) {
         return employeeRequestService.sendRequest(sendReqRequest);
     }
-
 }
