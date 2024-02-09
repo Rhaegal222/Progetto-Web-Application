96f25c952b9c1ddc55360d0df654d7b3bd9455c1
Rhaegal222 <rhaegal222@gmail.com>
2024-02-09 03:34:24 +0100

    Problema con gli ItemProxy che fanno crashare il backend ma non ci sono problemi

--- Frontend/src/app/employee-components/product-detail/product-detail.component.ts
+++ Frontend/src/app/employee-components/product-detail/product-detail.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { Product } from '../../model/product';
+import { User } from '../../model/user';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: [
     './product-detail.component.css',
-    '../../styles/grid.css',
-    '../../styles/list.css',
-    '../../styles/buttons.css',
+    '../../styles/buttons.css'
   ]
 })
 export class ProductDetailComponent {
@@ -23,7 +22,7 @@ export class ProductDetailComponent {
   description: string = '';
   location: string = '';
   image: string = '';
-  assignedUser: string = '';
+  assignedUser: User | undefined | string;
   assigned: boolean = false;  
   
   ngOnInit() {
@@ -31,13 +30,36 @@ export class ProductDetailComponent {
     const selectedProduct = localStorage.getItem('selectedProduct');
     if (selectedProduct !== null && selectedProduct !== undefined) {
       this.selectedProduct = JSON.parse(selectedProduct);
-      console.log(this.selectedProduct);
+
+      if(this.selectedProduct != null && this.selectedProduct != undefined){
+        if(this.selectedProduct.idItem != null && this.selectedProduct.idItem != undefined && this.selectedProduct.idItem != 0)
+          this.idItem = this.selectedProduct.idItem;
+        this.name = this.selectedProduct?.name;
+        this.type = this.selectedProduct?.type;
+        if(this.selectedProduct?.description != null && this.selectedProduct?.description != undefined && this.selectedProduct?.description != "")
+          this.description = this.selectedProduct?.description;
+        if(this.selectedProduct?.location != null && this.selectedProduct?.location != undefined && this.selectedProduct?.location != "")
+          this.location = this.selectedProduct.location;
+        this.image = this.selectedProduct?.image;
+        if(this.selectedProduct.assignedUser != null && this.assignedUser != undefined)
+          this.assignedUser = this.selectedProduct.assignedUser;
+        if(this.assignedUser != null && this.assignedUser != undefined && this.assignedUser != ""){
+          this.assigned = true;
+        }
+      }
     }
     
   }
 
   requestProduct(){
     
+  }
+
+  isAnImage(image : string): boolean {
+    if(image == null || image == "" || !image.startsWith('data:image/')){
+      return false;
+    }
+    return true;
   }
 
 }
