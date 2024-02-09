5f3f73ed507760069f598e883fab2ee86f65e76e
marteena <martinapiaimbrogno@gmail.com>
2024-02-09 11:47:55 +0100

    piccolo fix al proxy

--- Backend/src/main/java/it/unical/demacs/backend/Persistence/Dao/Postgres/UserProxy.java
+++ Backend/src/main/java/it/unical/demacs/backend/Persistence/Dao/Postgres/UserProxy.java
@@ -18,41 +18,41 @@ public class UserProxy extends User {
 
     @Override
     public ArrayList<Item> getItems() {
-        if(super.getItems() == null){
-            super.setItems(new ArrayList<>());
-            String query = "SELECT id_item FROM items WHERE assigned_user = ?";
-            try {
-                var st = this.con.prepareStatement(query);
-                st.setLong(1, getIdUser());
-                var rs = st.executeQuery();
-                while (rs.next()) {
-                    var item = DatabaseHandler.getInstance().getItemDao().findByPrimaryKey(rs.getLong(1)).join();
-                    super.getItems().add(item);
+            if(super.getItems() == null){
+                super.setItems(new ArrayList<>());
+                String query = "SELECT id_item FROM items WHERE assigned_user = ?";
+                try {
+                    var st = this.con.prepareStatement(query);
+                    st.setLong(1, getIdUser());
+                    var rs = st.executeQuery();
+                    while (rs.next()) {
+                        var item = DatabaseHandler.getInstance().getItemDao().findByPrimaryKey(rs.getLong(1)).join();
+                        super.getItems().add(item);
+                    }
+                } catch (Exception e) {
+                    e.fillInStackTrace();
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
             }
-        }
         return super.getItems();
     }
 
     @Override
     public ArrayList<EmployeeRequest> getEmployeeRequests() {
-        if(super.getEmployeeRequests() == null){
-            super.setEmployeeRequests(new ArrayList<>());
-            String query = "SELECT id_employee_request FROM employee_requests WHERE user = ?";
-            try {
-                var st = this.con.prepareStatement(query);
-                st.setLong(1, getIdUser());
-                var rs = st.executeQuery();
-                while (rs.next()) {
-                    var employeeRequest = DatabaseHandler.getInstance().getEmployeeRequestDao().findByPrimaryKey(rs.getLong(1)).join();
-                    super.getEmployeeRequests().add(employeeRequest);
+            if(super.getEmployeeRequests() == null){
+                super.setEmployeeRequests(new ArrayList<>());
+                String query = "SELECT id_employee_request FROM employee_requests WHERE user = ?";
+                try {
+                    var st = this.con.prepareStatement(query);
+                    st.setLong(1, getIdUser());
+                    var rs = st.executeQuery();
+                    while (rs.next()) {
+                        var employeeRequest = DatabaseHandler.getInstance().getEmployeeRequestDao().findByPrimaryKey(rs.getLong(1)).join();
+                        super.getEmployeeRequests().add(employeeRequest);
+                    }
+                } catch (Exception e) {
+                    e.fillInStackTrace();
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
             }
-        }
         return super.getEmployeeRequests();
     }
 }
