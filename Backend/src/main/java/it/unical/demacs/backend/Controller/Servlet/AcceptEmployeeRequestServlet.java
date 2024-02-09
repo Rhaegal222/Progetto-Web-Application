9d4fa5ccd9a02f405f19fc008db6b6673be95b02
marteena <martinapiaimbrogno@gmail.com>
2024-02-09 11:33:26 +0100

    eliminata servlet per accettare la richiesta

--- Backend/src/main/java/it/unical/demacs/backend/Controller/Servlet/AcceptEmployeeRequestServlet.java
+++
@@ -1,9 +0,0 @@
-package it.unical.demacs.backend.Controller.Servlet;
-
-import jakarta.servlet.annotation.WebServlet;
-import lombok.RequiredArgsConstructor;
-
-@WebServlet("/api/refuseRequest")
-@RequiredArgsConstructor
-public class AcceptEmployeeRequestServlet {
-}
